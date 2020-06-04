42193d351a66654f7dc462d0bbe7101149054b83
eric <eric@sonner.com.br>
2020-06-04 10:32:03 -0300

    Initial back-end tests

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+app.use(express.json());
 
-    response.json([
-        'Eric',
-        'Debora',
-        'Fredy',
-        'Pedro',
-    ]);
+const users = [
+    'Eric',
+    'Debora',
+    'Fredy',
+    'Pedro',
+]
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredusers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredusers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        nome: data.name,
+        email: data.email
+    }
+
+    return response.json(user);
 });
 
 app.listen(3333);
