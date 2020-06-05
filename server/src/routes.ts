a2564711f413410e339a1e7f511a0d17135c6861
eric <eric@sonner.com.br>
2020-06-05 11:06:34 -0300

    Initial values in items table and item-list route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello world' });
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/assets/${item.image}`,
+        }
+    });
+
+    return response.json(serializedItems);
 });
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/assets', express.static(path.resolve(__dirname, '..', 'assets')));
 
 app.listen(3333);
