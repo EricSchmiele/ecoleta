5f9dd430fa50c1656d56a7d2d053144871b47690
eric <eric@sonner.com.br>
2020-06-11 12:03:31 -0300

    CollectPoint form: map and images from back

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,129 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
 
 const CreatePoint = () => {
-    return <h1>Hello Planet</h1>;
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para home
+                </Link>
+            </header>
+
+            <form>
+                <h1>Cadastro do <br /> ponto de Coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp"
+                            />
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+
+                    <Map center={[-33.8673262, 151.2040589]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-33.8673262, 151.2040589]} />
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="state">Estado (UF)</label>
+                            <select name="state" id="state">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais ítens abaixo</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </fieldset>
+            </form>
+
+            <button type="submit">
+                Cadastrar ponto de coleta
+            </button>
+        </div>
+    );
 }
 
 export default CreatePoint;
